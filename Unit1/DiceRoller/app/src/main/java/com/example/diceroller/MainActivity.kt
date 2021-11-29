c86aabc3eeb2a7da1a41e97424eef15c45d9e499
qwerop553 <qwerop553@gmail.com>
2021-11-30 00:42:08 +0900

    Dice 2 Screen 구현 완료

--- Unit1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: MainActivityViewModel by viewModels()
 
-        setContent{
+        setContent {
             DiceActivityScreen(viewModel)
         }
 
@@ -31,20 +32,41 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun DiceActivityScreen(viewModel: MainActivityViewModel){
-    val result: Int by viewModel.number.observeAsState(1)
-    DiceScreen(result, viewModel::rollDice)
+fun DiceActivityScreen(viewModel: MainActivityViewModel) {
+    val result1: Int by viewModel.number1.observeAsState(1)
+    val result2: Int by viewModel.number2.observeAsState(-1)
+    Column(Modifier.fillMaxSize()) {
+
+        Row(Modifier.fillMaxWidth().weight(1f)) {
+            DiceScreen(Modifier.weight(1f), result1, viewModel::rollDice1)
+            if (result2 != -1) {
+                DiceScreen(Modifier.weight(1f), number = result2, onButtonClicked = viewModel::rollDice2)
+            }
+        }
+
+
+
+        Button(
+            onClick = if (result2 == -1) viewModel::makeDice2 else viewModel::deleteDice,
+            modifier = Modifier.align(CenterHorizontally).padding(16.dp)
+        ) {
+            if (result2 == -1)
+                Text("Make Dice 2")
+            else
+                Text("Delete Dice 2")
+        }
+    }
 }
 
 @Composable
-fun DiceScreen(number: Int, onButtonClicked: () -> Unit){
-    ConstraintLayout(Modifier.fillMaxSize()){
+fun DiceScreen(modifier: Modifier = Modifier, number: Int, onButtonClicked: () -> Unit) {
+    ConstraintLayout(modifier) {
 
         val (numberText, rollButton) = createRefs()
 
         Text(
             text = number.toString(),
-            modifier = Modifier.constrainAs(numberText){
+            modifier = Modifier.constrainAs(numberText) {
                 top.linkTo(parent.top, 0.dp)
                 bottom.linkTo(parent.bottom, 0.dp)
                 start.linkTo(parent.start, 0.dp)
@@ -54,12 +76,12 @@ fun DiceScreen(number: Int, onButtonClicked: () -> Unit){
 
         Button(
             onClick = onButtonClicked,
-            modifier = Modifier.constrainAs(rollButton){
+            modifier = Modifier.constrainAs(rollButton) {
                 top.linkTo(numberText.bottom, 0.dp)
                 start.linkTo(numberText.start, 0.dp)
                 end.linkTo(numberText.end, 0.dp)
             }
-        ){
+        ) {
             Text("ROLL")
         }
 
@@ -68,6 +90,6 @@ fun DiceScreen(number: Int, onButtonClicked: () -> Unit){
 
 @Preview
 @Composable
-fun DicePreview(){
-    DiceScreen(1){}
+fun DicePreview() {
+    DiceScreen(Modifier, 1) {}
 }
--- Unit1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivityViewModel.kt
+++ Unit1/DiceRoller/app/src/main/java/com/example/diceroller/MainActivityViewModel.kt
@@ -6,14 +6,32 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
 
-    private var _number: MutableLiveData<Int> = MutableLiveData(1)
-    val number: LiveData<Int> get() = _number
+    private var _number1: MutableLiveData<Int> = MutableLiveData(1)
+    val number1: LiveData<Int> get() = _number1
+
+    private var _number2: MutableLiveData<Int> = MutableLiveData(-1)
+    val number2: LiveData<Int> get() = _number2
 
     companion object{
         val dice = Dice(6)
     }
 
-    fun rollDice(){
-        _number.value = dice.roll()
+    fun rollDice1(){
+        _number1.value = dice.roll()
+    }
+
+    /**
+     * Create Dice 2 when user clicked [New Dice] Button.
+     */
+    fun makeDice2(){
+        _number2.value = 1
+    }
+
+    fun deleteDice(){
+        _number2.value = -1
+    }
+
+    fun rollDice2(){
+        _number2.value = dice.roll()
     }
 }
